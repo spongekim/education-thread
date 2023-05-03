5c9bddf666f9fd7c4540fecf46446f2cbd7995b8
spongekim <46959619+spongekim@users.noreply.github.com>
2023-05-04 08:54:40 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# education-thread
+# education_thread
+성능 WG 교육안 실습 내용 (process/thread)
---
+++ proj01/user_solution.cpp
@@ -0,0 +1,21 @@
+#include "workitem.h"
+#include "workerthread.h"
+#include <future>
+
+
+void user_init(int max_produce_)
+{
+
+}
+
+void user_consume(std::unique_ptr<WorkItem> workitem)
+{
+
+}
+
+void user_deinit(void)
+{
+
+}
+
+
